657bd59a46224fb2526b7f0ffb822ddf2ba1b78e
Basselieus <131277298+Basselieus@users.noreply.github.com>
2024-06-23 16:27:19 +0200

    added DataCollectionReceiver

---
+++ DataCollectionReceiver/src/main/java/fhtechnikum/com/example/datacollectionreceiver/communication/Receiver.java
@@ -0,0 +1,45 @@
+package fhtechnikum.com.example.datacollectionreceiver.communication;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Receiver {
+
+    public static void receive(String queueName, String brokerUrl) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(brokerUrl);
+        factory.setPort(30003);
+
+        Connection rabbitConnection = factory.newConnection();
+        Channel channel = rabbitConnection.createChannel();
+
+        channel.queueDeclare(queueName, false, false, false, null);
+        System.out.println(" [x] Listening on queue '" + queueName + "' at broker '" + brokerUrl + "'");
+
+        StringBuilder collectedData = new StringBuilder();
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received message: " + message);
+
+            if (message.endsWith("end")) {
+                // Send collected data to the PDF_GENERATOR queue
+                Sender.send(collectedData.toString(), "PDF_GENERATOR", brokerUrl);
+                System.out.println(" [x] Sent collected data to PDF_GENERATOR queue");
+                // Reset the collected data
+                collectedData.setLength(0);
+            } else {
+                // Add data to collectedData
+                collectedData.append(message).append(";");
+            }
+        };
+
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {});
+        System.out.println(" [x] Consumer started and waiting for messages...");
+    }
+}
---
+++ DataCollectionReceiver/src/main/java/fhtechnikum/com/example/datacollectionreceiver/communication/Sender.java
@@ -0,0 +1,23 @@
+package fhtechnikum.com.example.datacollectionreceiver.communication;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Sender {
+    public static void send(String msg, String queueName, String brokerUrl) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(brokerUrl);
+        factory.setPort(30003);
+
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(queueName, false, false, false, null);
+            channel.basicPublish("", queueName, null, msg.getBytes());
+            System.out.println(" [x] Sent '" + msg + "' to queue: " + queueName);
+        } catch (Exception e) {
+            System.out.println(" [x] Unexpected exception at Sender: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
